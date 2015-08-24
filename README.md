ab35c1c136e47415452944839e7c9be9f687e265
Lingxiao Sun <sunny9732@gmail.com>
2015-08-24 12:11:17 -0400

    update package and document

---
+++ README.md
@@ -0,0 +1,13 @@
+Meteor jdenticon
+=======
+
+About
+----------------
+This ia a [meteor](https://www.meteor.com/) package which contains lastest version of [jdenticon](https://jdenticon.com/)
+
+Installation
+----------------
+
+```
+meteor add ls3271:meteor-jdenticon
+```
--- package.js
+++ package.js
@@ -1,22 +1,16 @@
 Package.describe({
-  name: 'meteor-jdenticon',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'ls3271:meteor-jdenticon',
+	version: '0.0.2',
+	// Brief, one-line summary of the package.
+	summary: 'This is a Meteor library for generating identicons from a hash using canvas.',
+	// URL to the Git repository containing the source code for this package.
+	git: 'https://github.com/sunny0825/meteor-jdenticon.git',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.addFiles('meteor-jdenticon.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('meteor-jdenticon');
-  api.addFiles('meteor-jdenticon-tests.js');
+	api.versionsFrom('1.1.0.3');
+	api.add_files('jdenticon/jdenticon.js', 'client');
 });
